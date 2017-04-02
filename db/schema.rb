185221494f6fdb10aa1703676e815d14cd0f6963
jmerchan <merchan1395@gmail.com>
2017-04-01 22:49:44 -0400

    Refactor Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170401185030) do
+ActiveRecord::Schema.define(version: 20170402024759) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
+    t.string   "document",                            null: false
+    t.string   "name",                                null: false
+    t.string   "lastname",                            null: false
+    t.string   "phone",                               null: false
+    t.string   "role",                                null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -25,6 +30,9 @@ ActiveRecord::Schema.define(version: 20170401185030) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "auth_token"
+    t.index ["auth_token"], name: "index_users_on_auth_token"
+    t.index ["document"], name: "index_users_on_document", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
