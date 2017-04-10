756c69863d0e445f8e8091b0394d1f0180d51cd0
jmerchan <merchan1395@gmail.com>
2017-04-10 19:32:59 -0400

    Implementation of adding a insurance & occupation (complete)

--- app/controllers/medical_records_controller.rb
+++ app/controllers/medical_records_controller.rb
@@ -39,7 +39,8 @@ class MedicalRecordsController < ApplicationController
     def record_params
       params.require(:medical_record).permit( :document, :document_type, :first_consultation_date, :name, :last_name,
                                               :birth_date, :gender, :phone_number, :cellphone_number, :address, :email,
-                                              :referred_by, :profile_picture, :representative_document )
+                                              :referred_by, :profile_picture, :representative_document, :occupation_id,
+                                              :insurance_id )
     end
 
     def set_record
--- app/models/medical_record.rb
+++ app/models/medical_record.rb
@@ -1,8 +1,8 @@
 class MedicalRecord < ApplicationRecord
   has_many :user_medical_records
   has_many  :users, through: :user_medical_records
-  belongs_to :insurance
-  belongs_to :occupation
+  belongs_to :insurance, optional: true
+  belongs_to :occupation, optional: true
 
   validates_presence_of :document, :document_type, :first_consultation_date, :name, :last_name, :birth_date, :gender,
                         :phone_number, :address
