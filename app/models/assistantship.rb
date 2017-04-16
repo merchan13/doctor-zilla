3a76558f84228876369ff675d6c62b896dc78df0
jmerchan <merchan1395@gmail.com>
2017-04-16 01:30:34 -0400

    Assistantship bug fix

--- app/models/assistantship.rb
+++ app/models/assistantship.rb
@@ -1,5 +1,4 @@
 class Assistantship < ApplicationRecord
   belongs_to :user
-  #belongs_to :doctor, :class_name => 'User'
   belongs_to :assistant, :class_name => 'User'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170415021147) do
+ActiveRecord::Schema.define(version: 20170416052437) do
 
   create_table "afflictions", force: :cascade do |t|
     t.string   "name",        null: false
