547c9e5c2dcf3273794718854a2c8f36e96ead13
jmerchan <merchan1395@gmail.com>
2017-04-19 11:57:14 -0400

    Delete drop migrations

--- db/migrate/20170418025352_drop_affliction.rb
+++
@@ -1,5 +0,0 @@
-class DropAffliction < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :afflictions
-  end
-end
--- db/migrate/20170418032205_drop_basic_exams.rb
+++
@@ -1,5 +0,0 @@
-class DropBasicExams < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :basic_exams
-  end
-end
