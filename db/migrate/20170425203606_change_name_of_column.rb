38ecb92851b42f1bd504598e03bd18fd8e95e442
jmerchan <merchan1395@gmail.com>
2017-04-27 02:08:37 -0400

    Fix migration issuis for production

--- db/migrate/20170425203606_change_name_of_column.rb
+++ db/migrate/20170425203606_change_name_of_column.rb
@@ -1,6 +1,5 @@
 class ChangeNameOfColumn < ActiveRecord::Migration[5.0]
   def change
     add_column :prescriptions, :interval_unit, :float
-    remove_column :prescriptions, :intervalo_unit, :float
   end
 end
