b165cce3a375cf93d65cf5652f7b6bcd9fb66702
jmerchan <merchan1395@gmail.com>
2017-05-04 00:22:55 -0400

    Form fix in the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,10 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    #devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :document, :name, :lastname, :phone, :role, :password, :password_confirmation, :avatar, :avatar_cache) }
-    #devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:email, :name, :lastname, :phone, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar) }
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
       user_params.permit(:email, :document, :name, :lastname, :phone, :role, :password, :password_confirmation, :avatar, :avatar_cache)
     end
-    
+
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
       user_params.permit(:email, :name, :lastname, :phone, :password, :password_confirmation, :current_password, :avatar, :avatar_cache, :remove_avatar)
     end
