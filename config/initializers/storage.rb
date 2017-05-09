6d2ad904ce04490aea0f7ce55602f040d7fc96e0
jmerchan <merchan1395@gmail.com>
2017-05-08 22:53:44 -0400

    Fog storage fix part 2.2

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,4 +1,14 @@
 CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+      provider:              'AWS',
+      aws_access_key_id:     ENV["aws_access_id"],
+      aws_secret_access_key: ENV["aws_access_secret_key"],
+      region: 'sa-east-1'
+  }
+  config.fog_directory  = "doctorzilla"
+  config.fog_public     = false
+
   # Use local storage if in development or test
   if Rails.env.development? || Rails.env.test?
     CarrierWave.configure do |config|
@@ -12,14 +22,4 @@ CarrierWave.configure do |config|
       config.storage = :fog
     end
   end
-
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-      provider:              'AWS',
-      aws_access_key_id:     ENV["aws_access_id"],
-      aws_secret_access_key: ENV["aws_access_secret_key"],
-      region: 'sa-east-1'
-  }
-  config.fog_directory  = "doctorzilla"
-  config.fog_public     = false
 end
