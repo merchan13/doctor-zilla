2ff54cd6f6002bb014138c32a38cc340a3fa29a5
jmerchan <merchan1395@gmail.com>
2017-07-21 00:57:11 -0400

    Attachments upload system improvements

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -1,19 +1,49 @@
 class AttachmentsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :create
+  before_action :set_medical_record, only: [:new, :create, :index]
+  before_action :set_attachment, only: [:destroy]
+
+  def index
+    @attachments = @record.attachments.order(created_at: :desc).paginate(page: params[:page], per_page: 20)
+  end
+
+  def new
+    @attachment = @record.attachments.new
+  end
 
   def create
-    @attachment = Attachment.create(attachment_params)
+    Attachment.transaction do
+      @attachment = @record.attachments.create(attachment_params)
 
-    if @attachment.save
-      flash[:success] = "New attachment added"
-    else
-      #render status: :not_found, nothing: true
+      if @attachment.save
+        flash[:success] = "New attachment added"
+        redirect_to attachments_path(record: @record)
+      else
+        render 'new'
+      end
+    end
+  end
+
+  def destroy
+    Attachment.transaction do
+      @record = @attachment.medical_record
+
+      @attachment.destroy
+
+      flash[:success] = "Attachment deleted"
+      redirect_to attachments_path(record: @record)
     end
   end
 
   private
     def attachment_params
-      params.require(:attachment).permit( :description, :url, :url_cache, :remove_url, :medical_record_id )
+      params.require(:attachment).permit( :description, :url, :url_cache, :remove_url)
     end
 
+    def set_attachment
+      @attachment = Attachment.find(params[:id])
+    end
+
+    def set_medical_record
+      @record = MedicalRecord.find(params[:record])
+    end
 end
